727fd2836f768cfebb8e9195b7ad4c5dc6248c14
Alexander Golofeev <alex00x6@gmail.com>
2016-12-05 11:51:29 +0200

    test of circle.yml

--- main/java/api/urls/ApiUrls.java
+++ main/java/api/urls/ApiUrls.java
@@ -4,7 +4,9 @@ package api.urls;
 public enum ApiUrls {
 
     BASE("http://aristo-pets.com"),
+    ADVERTS("/api/adverts"),
     BREEDS("/api/breeds"),
+    USER("/api/user"),
     ANIMAL("/api/animal");
 
     private String uri;
