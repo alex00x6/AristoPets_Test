b780a8bde7b4bc36a7b87a5047f625660fc17173
Alexander Golofeev <alex00x6@gmail.com>
2017-01-27 20:21:16 +0200

    added all routes in requestgroups

--- src/main/java/api/utils/Helpers.java
+++ src/main/java/api/utils/Helpers.java
@@ -7,9 +7,7 @@ import java.security.SecureRandom;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
-/**
- * Created by Storm on 27.01.2017.
- */
+
 public class Helpers {
 
     public String randomString(){
@@ -19,22 +17,22 @@ public class Helpers {
     }
 
     public int randomInt(){
-        int rand = Math.toIntExact(generateRandomNumber(6));
-
+        int length = 6;
+        int rand = Math.toIntExact(generateRandomNumber(length));
         return rand;
     }
 
 
-    private String createRandomText(int codeLength, String id) {
+    private String createRandomText(int length, String id) {
         return new SecureRandom()
-                .ints(codeLength, 0, id.length())
+                .ints(length, 0, id.length())
                 .mapToObj(id::charAt)
                 .map(Object::toString)
                 .collect(Collectors.joining());
     }
 
-    private long generateRandomNumber(int n) {
-        long min = (long) Math.pow(10, n - 1);
+    private long generateRandomNumber(int length) {
+        long min = (long) Math.pow(10, length - 1);
         return ThreadLocalRandom.current().nextLong(min, min * 10);
     }
 
